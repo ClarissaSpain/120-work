60be18940e070cc4440d226aae14973160483cac
Clarissa <cs113233@umconnect.umt.edu>
2018-03-06 19:55:58 -0700

    practice project

---
+++ Character_Generator/sketch.js
@@ -0,0 +1,33 @@
+// let hats
+// let Goggles
+// let Masks
+// let Age
+// let Gender
+// let Jacket
+// let Shoes
+// let Pants
+// let Scarf
+// let Gloves
+
+// var hats =[ 'Baseball', 'cowboy', 'Beanie', 'Sunhat', 'leather cap', 'Robinhood', 'Bomber', 'Fur', 'Hoodie', 'Tillie', 'Santa', 'Fedora', 'Newsboy', 'Beret', 'Bowler', 'Cloche', 'dearstalker', 'Cocktail', 'None'];
+// var Goggles = [ 'Yes', 'No'];
+// var Masks = ['Surgeon', 'Gasmask', 'Scarf', 'Rebreather', 'Japanese', 'Visor', 'None'];
+// var Age= ['Child', 'Adult', 'Old'];
+// var Gender = ['Male', 'Female'];
+// var Jacket = [ 'Yes', 'No', 'Pancho'];
+// var Shoes = ['Boots', 'Shoes', 'None'];
+// var Pants =['Pants', 'Skirt', 'Cargo Pants'];
+// var Scarf = [ 'Yes', 'No'];
+// var Gloves = [ 'Yes', 'No'];
+
+function setup() {
+  createCanvas(600, 800);
+}
+
+function draw(){
+  background('#b3dcc9');
+text size(32);
+var hat = random(hats);
+text("Hat:" + hats, 10, 30);
+
+}

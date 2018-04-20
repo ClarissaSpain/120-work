e205f61816c67071b66a0aedfe0a38012991fbb8
Clarissa <cs113233@umconnect.umt.edu>
2018-04-19 20:17:12 -0600

    rename

--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -1,31 +1,58 @@
-var bug;
+var ship;
+var flowers = [];
+var drops = [];
 
 function setup(){
-  createCanvas(600, 600);
-  background('grey');
-  // create object and pass in parameters
-  bug = new JitterBug(width/2, height/2, 20);
-
+  createCanvas(800, 600);
+  ship = new Ship();
+  drop = new Drop(width/2, height/2);
+  for (var i = 0; i < 9; i++){
+    flowers[i] = new Flower(i*80+80, 60);
+  }
 }
 
 function draw(){
-  bug.move();
-  bug.display();
+background('#86ed7d');
+ship.show();
+ship.move();
+for (var i = 0; i < drops.length; i++){
+  drops[i].show();
+  drops[i].move();
+  for (var j = 0; j < flowers.length; j++){
+    if (drops[i].hits(flowers[j])) {
+      flowers[j].grow();
+      drops[i].evaporate();
+      }
+    }
+  }
+
+var edge = false;
+
+for (var i = 0; i < flowers.length; i++){
+  flowers[i].show();
+  flowers[i].move();
+  if (flowers[i].x > width || flowers[i]. x < 0){
+    edge = true;
+  }
 
 }
 
-function JitterBug(tempX, tempY, tempDiameter){
-  this.x = tempX;
-  this.y = tempY;
-  this.diameter = tempDiameter;
-  this.speed = 2.5;
+if (edge) {
+  for (var i = 0; i < flowers.length; i++){
+    flowers[i].shiftDown();
+  }
+}
 
-  this.move = function(){
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
+
+// walking through the array backwards so that I don't skip elements.
+for (var i = drops.length-1; i >= 0; i--){
+  if (drops[i].toDelete){
+    drops.splice(i, 1);
+    }
   }
-  this.display = function(){
-    fill('yellow');
-    ellipse(this.x, this.y, this.diameter, this. diameter);
-  }
+}
+
+function mousePressed(){
+  var drop = new Drop(ship.x, height);
+  drops.push(drop);
 }
